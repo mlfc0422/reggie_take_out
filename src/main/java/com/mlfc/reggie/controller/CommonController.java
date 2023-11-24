2de7cdfc4add35541c8b04da092e6c66cbe1cd9b
mlfc <mlfc0422@outlook.com>
2023-11-24 19:43:21 +0800

    上传文件

---
+++ src/main/java/com/mlfc/reggie/controller/CommonController.java
@@ -0,0 +1,76 @@
+package com.mlfc.reggie.controller;
+
+import com.mlfc.reggie.common.Rest;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+@RestController
+@RequestMapping("/common")
+public class CommonController {
+
+    @Value("${reggie.path}")
+    private String basePath;
+
+    /**
+     * 上传文件
+     */
+    @PostMapping("/upload")
+    public Rest<String> upload(MultipartFile file){
+        log.info("上传文件");
+        log.info(file.toString());
+
+        String originalFilename = file.getOriginalFilename();
+        String substring = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String fileName = UUID.randomUUID().toString()+substring;
+
+        File fileDir = new File(basePath);
+        if (!fileDir.exists()){
+            fileDir.mkdirs();
+        }
+
+        try {
+            file.transferTo(new File(basePath+fileName));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return Rest.success(fileName);
+    }
+
+    /**
+     * 下载文件
+     */
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response){
+        try {
+            FileInputStream fileInputStream = new FileInputStream(new File(basePath + name));
+            ServletOutputStream outputStream = response.getOutputStream();
+
+            response.setContentType("image/jpeg");
+            int len = 0;
+            byte[] bytes = new byte[1024];
+
+            while ((len = fileInputStream.read(bytes)) != -1){
+                outputStream.write(bytes,0,len);
+                outputStream.flush();
+            }
+            outputStream.close();
+            fileInputStream.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
